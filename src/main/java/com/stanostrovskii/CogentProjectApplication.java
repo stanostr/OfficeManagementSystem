274a2771b4b48c74f4899bd783cb17164ce8d1f2
stanostr <stan_ostr@yahoo.com>
2019-07-02 17:07:18 -0400

    Created project. First commit.

---
+++ src/main/java/com/stanostrovskii/CogentProjectApplication.java
@@ -0,0 +1,13 @@
+package com.stanostrovskii;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CogentProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CogentProjectApplication.class, args);
+	}
+
+}
