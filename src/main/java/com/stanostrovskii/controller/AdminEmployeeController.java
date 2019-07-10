2d489b2b9823af798fb77aee5d38f082e0a5c11c
stanostr <stan_ostr@yahoo.com>
2019-07-09 23:57:01 -0400

    Code cleanup

--- src/main/java/com/stanostrovskii/controller/AdminEmployeeController.java
+++ src/main/java/com/stanostrovskii/controller/AdminEmployeeController.java
@@ -1,16 +1,11 @@
 package com.stanostrovskii.controller;
 
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -25,12 +20,8 @@ import org.springframework.http.HttpStatus;
 
 import com.stanostrovskii.dao.DepartmentRepository;
 import com.stanostrovskii.dao.EmployeeRepository;
-import com.stanostrovskii.dao.TaskRepository;
-import com.stanostrovskii.model.Admin;
 import com.stanostrovskii.model.Department;
 import com.stanostrovskii.model.Employee;
-import com.stanostrovskii.model.Task;
-import com.stanostrovskii.model.TaskForm;
 import com.stanostrovskii.service.EmailService;
 import com.stanostrovskii.util.EmployeeEmailUtil;
 
